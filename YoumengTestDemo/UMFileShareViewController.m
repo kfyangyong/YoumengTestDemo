f71a41d7a8f37203816482bdab70e2e447fc4dae
ayong <ayongxixi@163.com>
2017-01-16 17:32:09 +0800

    分享文件

--- YoumengTestDemo/UMFileShareViewController.m
+++ YoumengTestDemo/UMFileShareViewController.m
@@ -7,16 +7,113 @@
 //
 
 #import "UMFileShareViewController.h"
+//友盟分享
+#import <UMSocialCore/UMSocialCore.h>
+#import <UShareUI/UShareUI.h>
 
-@interface UMFileShareViewController ()
+@interface UMFileShareViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic,strong) NSArray *dataArr;
+@property (nonatomic,strong) NSArray *shareData;
 
 @end
 
 @implementation UMFileShareViewController
 
+static NSString *const fileName = @"123"; //文件名
+
+- (NSArray *)dataArr{
+    if (_dataArr == nil) {
+        
+        _dataArr = [self getFileArr];
+    }
+    return _dataArr;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    NSArray *arr = self.dataArr;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+}
+
+/** 获取文件名*/
+- (NSArray *)getFileArr{
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+   
+    NSString *filePath = [docDir stringByAppendingPathComponent:fileName];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSArray *fileArr = [fileManager contentsOfDirectoryAtPath:filePath error:nil];
+    NSArray *fileList = [[fileManager contentsOfDirectoryAtPath:filePath error:nil] pathsMatchingExtensions:[NSArray arrayWithObject:@"db"]];
+    return fileArr;
+}
+
+
+#pragma mark - uitableViewDatasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.dataArr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = self.dataArr[indexPath.row];
+    return cell;
+}
+
+#pragma mark - uitableViewdelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    self.shareData = @[self.dataArr[indexPath.row]];
+    //显示分享面板
+    [UMSocialUIManager showShareMenuViewInWindowWithPlatformSelectionBlock:^(UMSocialPlatformType platformType, NSDictionary *userInfo) {
+        [self getUserInfoForPlatform:platformType];
+        [self shareImageToPlatformType:platformType];
+        
+    }];
+}
+
+//获取用户授权
+- (void)getUserInfoForPlatform:(UMSocialPlatformType)platformType
+{
+    [[UMSocialManager defaultManager] getUserInfoWithPlatform:platformType currentViewController:self completion:^(id result, NSError *error) {
+        
+        UMSocialUserInfoResponse *resp = result;
+        
+        // 第三方登录数据(为空表示平台未提供)
+        // 授权数据
+        NSLog(@" uid: %@", resp.uid);
+        NSLog(@" openid: %@", resp.openid);
+        NSLog(@" accessToken: %@", resp.accessToken);
+        NSLog(@" refreshToken: %@", resp.refreshToken);
+        NSLog(@" expiration: %@", resp.expiration);
+        
+        // 用户数据
+        NSLog(@" name: %@", resp.name);
+        NSLog(@" iconurl: %@", resp.iconurl);
+        NSLog(@" gender: %@", resp.gender);
+        
+        // 第三方平台SDK原始数据
+        NSLog(@" originalResponse: %@", resp.originalResponse);
+    }];
+    
+}
+//分享文本内容
+- (void)shareImageToPlatformType:(UMSocialPlatformType)platformType
+{
+    //创建分享消息对象
+    UMSocialMessageObject *messageObject = [UMSocialMessageObject messageObject];
+    //设置文本
+    messageObject.shareObject = self.shareData.firstObject;
+    //调用分享接口
+    [[UMSocialManager defaultManager] shareToPlatform:platformType messageObject:messageObject currentViewController:self completion:^(id data, NSError *error) {
+        if (error) {
+            NSLog(@"************Share fail with error %@*********",error);
+        }else{
+            NSLog(@"response data is %@",data);
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
